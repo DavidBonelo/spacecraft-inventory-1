fcbcb6649b39074ce9813b23a4c986a5541bdffa
David Bonelo <bonelodev@gmail.com>
2023-01-12 23:52:36 -0500

    add inventory init data

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,10 +1,27 @@
-import model.Spacecraft;
+import model.*;
 
 import java.util.ArrayList;
 import java.util.Objects;
 
 public class Inventory {
     private static final ArrayList<Spacecraft> spacecrafts = new ArrayList<Spacecraft>();
+
+    public static void initData() {
+        if (spacecrafts.size() < 12) {
+            spacecrafts.add(new LaunchSpacecraft("a"));
+            spacecrafts.add(new NonTripulatedSpacecraft("a"));
+            spacecrafts.add(new TripulatedSpacecraft("a"));
+            spacecrafts.add(new Orbiter("a"));
+            spacecrafts.add(new LaunchSpacecraft("b"));
+            spacecrafts.add(new NonTripulatedSpacecraft("b"));
+            spacecrafts.add(new TripulatedSpacecraft("b"));
+            spacecrafts.add(new Orbiter("b"));
+            spacecrafts.add(new LaunchSpacecraft("c"));
+            spacecrafts.add(new NonTripulatedSpacecraft("c"));
+            spacecrafts.add(new TripulatedSpacecraft("c"));
+            spacecrafts.add(new Orbiter("c"));
+        }
+    }
 
     public static ArrayList<Spacecraft> getSpacecrafts() {
         return spacecrafts;
