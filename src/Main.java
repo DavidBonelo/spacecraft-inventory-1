3f7f3419935f60c80f95d4985fc1a073e285db0a
David Bonelo <bonelodev@gmail.com>
2023-01-12 23:43:04 -0500

    add showInventory and search

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,51 @@
+import model.Spacecraft;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Spacecraft Inventory");
 
+        Inventory.initData();
+
         boolean exit = false;
         while (!exit) {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Menu: 1. Show Inventory | 2. Search spacecraft | 3. Create spacecraft | 4. Delete spacecraft | 5. Update spacecraft | 6. Exit");
+            System.out.println("Menu: 1. Show Inventory | 2. Search spacecraft | 3. Create spacecraft | 4. Delete spacecraft | 5. Update spacecraft | 0. Exit");
             int menuChoice = scanner.nextInt();
             switch (menuChoice) {
-                case 1 -> showInventory();
+                case 1 -> showInventory(Inventory.getSpacecrafts());
                 case 2 -> searchSpacecraft();
 //                case 3 -> createSpacecraft();
 //                case 4 -> deleteSpacecraft();
 //                case 5 -> updateSpacecraft();
-                case 6 -> exit = true;
+                case 0 -> exit = true;
                 default -> System.out.println("Unknown menu option");
             }
         }
     }
 
-    private static void showInventory() {
+    private static void showInventory(ArrayList<Spacecraft> spacecrafts) {
         // print inventory // add pagination later
         System.out.println("*inserte el inventario aquí*");
+        for (int i = 0; i < spacecrafts.size(); i++) {
+            Spacecraft sc = spacecrafts.get(i);
+            System.out.println(i + " | name: " + sc.name + " | type: " + sc.type);
+        }
     }
 
     private static void searchSpacecraft() {
-        String[] searchOptions = {"name", "launchDate", "endOfServiceDate", "active", "combustibleType", "weight", "thrust", "height", "speed"};
+//        String[] searchOptions = {"name", "launchDate", "endOfServiceDate", "active", "combustibleType", "weight", "thrust", "height", "speed"};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What type of spacecraft do you want to search? : 1. Launch | 2. NonTripulated | 3. Tripulated | 4. Orbiter | 5. Probe");
+        int menuChoice = scanner.nextInt();
+        switch (menuChoice) {
+            case 1 -> showInventory(Inventory.searchItem("launch"));
+            case 2 -> showInventory(Inventory.searchItem("nontripulated"));
+            case 3 -> showInventory(Inventory.searchItem("tripulated"));
+            case 4 -> showInventory(Inventory.searchItem("orbiter"));
+            case 5 -> showInventory(Inventory.searchItem("probe"));
+        }
     }
 }
